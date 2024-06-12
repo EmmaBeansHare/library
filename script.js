8b2b58aea49cbd9248687e0eb9b2cf5d05c4ddbd
Emma Hare <BBOT@Beans-M2Pro.local>
2024-06-12 19:50:56 +0200

    Finish HTML and CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+const bookBtn = document.getElementById("bookBtn");
+const gridContainer = document.getElementById("gridContainer");
+const formModal = document.getElementById("formModal");
+const addBookForm = document.getElementById("addBookForm");
+const titleInput = document.getElementById("titleInput");
+const authorInput = document.getElementById("authorInput");
+const pagesInput = document.getElementById("pagesInput");
+const readStatus = document.getElementById("readStatus");
+const submitBtn = document.getElementById("submitBtn");
+
+const readValue = document.querySelector("#readStatus:checked");
+
+function myBook(){
+    
+}
+    
+
